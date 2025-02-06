30c88ae1a1943fb77e0bd699d933725c7a4915bb
thothcher <g.vakhtangishvili@itstep.ge>
2025-02-06 23:00:31 +0400

    'newnw'

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Fixes newlines
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
 
 const db = admin.firestore();
-
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
